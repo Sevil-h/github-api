e2420bd743f7177c1c0d780d672e7cba7aaa597a
Sabrie Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-02-16 17:29:06 +0300

    added spinner

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,32 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Spinner from "../layout/Spinner";
 
 function UserResults() {
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const fetchUsers = () => {
+  useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_GITHUB_URL}/users`)
       .then((res) => {
         const data = res.data;
-        console.log(data);
+        setUsers(data);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
       });
-  };
-  // const fetchUsers = async () => {
-  //   const response = await fetch(`${process.env.REACT_GITHUB_URL}/users`, {
-  //     headers: {
-  //       Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-  //     },
-  //   });
-  //   const data = await response.json();
-  //   console.log(data);
-  // };
-  return <div>user results</div>;
+  }, []);
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        <ul>
+          {users.map((user) => (
+            <p key={user.id}>{user.login}</p>
+          ))}
+        </ul>
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
 }
 
 export default UserResults;
