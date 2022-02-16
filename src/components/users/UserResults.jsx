ece0b2748d6d5419e059ef008a344db7da57b88b
Sabrie Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-02-16 16:59:29 +0300

    small changes

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 
 function UserResults() {
   useEffect(() => {
-    console.log(`${process.env.REACT_APP_GITHUB_URL}`);
     fetchUsers();
   }, []);
 
@@ -18,6 +17,15 @@ function UserResults() {
         console.log(err);
       });
   };
+  // const fetchUsers = async () => {
+  //   const response = await fetch(`${process.env.REACT_GITHUB_URL}/users`, {
+  //     headers: {
+  //       Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+  //     },
+  //   });
+  //   const data = await response.json();
+  //   console.log(data);
+  // };
   return <div>user results</div>;
 }
 
